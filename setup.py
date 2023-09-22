303355dd26363df49ffed0f8d905c04040ca448a
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2023-09-22 14:01:45 -0400

    lint: update+run precommit

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
 import setuptools
 
-setuptools.setup(use_scm_version={"write_to": "napari_broadcastable_points/_version.py"})
+setuptools.setup(
+    use_scm_version={"write_to": "napari_broadcastable_points/_version.py"}
+)
